b82c6de128e5a843d8cc4ea502db8a13f6ef6d2c
Campione <alfrecampione@gmail.com>
2022-11-07 22:17:13 -0500

    Added some image control

--- testing_pil.py
+++ testing_pil.py
@@ -2,9 +2,22 @@ from PIL import Image
 import os, glob
 import numpy as np
 
-size = 128, 128
+
+def rgb2hex(r, g, b):
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+
+def Is_Green(hex_color):
+    # TODO Get the bounds of green in hex
+    return True
+
+
 scale = 0
+pixel_amounts = []
+count = 0
 for infile in glob.glob("Photos/*.jpg", recursive=True):
+    scale = input("Please enter the scale: ")
+    pixel_amounts.append(0)
     file, ext = os.path.splitext(os.path.basename(infile))
     dir_name = os.path.dirname(infile)
     with Image.open(infile, mode="r", formats=None) as im:
@@ -12,5 +25,10 @@ for infile in glob.glob("Photos/*.jpg", recursive=True):
         img_nump = np.zeros(shape=(height, width))
         for h in range(height):
             for w in range(width):
-                pixel = im.getpixel((h, w))
+                rgb_color = im.getpixel((h, w))
                 # TODO search for green palet colors
+                r, g, b = rgb_color
+                hex_color = rgb2hex(r, g, b)
+                if Is_Green(hex_color):
+                    pixel_amounts[count] += 1
+    count += 1
